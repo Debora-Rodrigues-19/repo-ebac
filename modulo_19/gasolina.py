140632f75fefb0364970f545760eef3c4920b6f8
contato <debora9rodrigues@gmail.com>
2024-03-31 18:55:47 -0300

    Novo gráfico

--- modulo_19/gasolina.py
+++ modulo_19/gasolina.py
@@ -1,8 +1,7 @@
-# código de geração do gráfico
 import pandas as pd
 dado = pd.read_csv ('gasolina.csv')
 
-grafico = dado.plot.line(x='dia', y='venda', xlabel='Dia', ylabel='Venda', title='Preço médio da gasolina na cidade de São Paulo, 10 primeiros dias, Jul/21')
+grafico = dado.plot.line(color='green', x='dia', y='venda', xlabel='Dia', ylabel='Vendas', title='Preço médio da gasolina na cidade de SP, 10 primeiros dias, Jul/21')
 
 figura = grafico.get_figure()
 figura.savefig('grafico_gasolina.png')
